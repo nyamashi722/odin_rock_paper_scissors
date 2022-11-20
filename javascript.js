daa44dd27f0a4f672eeeec7590df4f88ccb75e01
nyamashi722 <yamashiro.nao@hotmail.com>
2022-11-20 17:23:49 -0500

    change image through button click

--- javascript.js
+++ javascript.js
@@ -1,28 +1,32 @@
-function getComputerChoice() {
-    let result = Math.random();
-    
-    if (result <= (1/3)) {
-      let computerSelection = "rock";
-      return computerSelection;
-    } else if (result <= (2/3)) {
-      let computerSelection = "paper"; 
-      return computerSelection;
-    } else {
-      let computerSelection = "scissors";
-      return computerSelection;
-    }
-};
-
 let playerScore = 0;
 let computerScore = 0;
-
 let userChoice = "";
 
 const buttons = document.querySelectorAll("button");
+const results = document.querySelector("#results-container")
+const score = document.querySelector("#score-container")
+const finalResult = document.querySelector("#final-result")
+const playerSymbol = document.querySelector("#player-symbol")
+const computerSymbol = document.querySelector("#computer-symbol")
+
+function getComputerChoice() {
+  let result = Math.random();
+  if (result <= (1/3)) {
+    let computerSelection = "rock";
+    return computerSelection;
+  } else if (result <= (2/3)) {
+    let computerSelection = "paper"; 
+    return computerSelection;
+  } else {
+    let computerSelection = "scissors";
+    return computerSelection;
+  }
+};
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     userChoice = (button.value)
+    playerSymbolContent.src = `${userChoice}.png`;
   })
 });
 
@@ -30,51 +34,61 @@ buttons.forEach((button) => {
   button.addEventListener("click", game)
 });
 
-const results = document.querySelector("#results-container")
 
 const resultsContent = document.createElement("div");
 resultsContent.classList.add("results-content");
-
 results.appendChild(resultsContent);
 
+const playerSymbolContent = document.createElement("img")
+const computerSymbolContent = document.createElement("img")
+
 function playRound(playerSelection, computerSelection) {
-   if (playerSelection == computerSelection) {
-      resultsContent.textContent = `It's a tie! You both chose ${playerSelection}!`
-      return resultsContent.textContent
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-      resultsContent.textContent = "You lose! Paper beats Rock!"
-      return resultsContent.textContent
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-      resultsContent.textContent = "You win! Rock beats scissors!"
-      return resultsContent.textContent
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-      resultsContent.textContent = "You win! Paper beats rock!"
-      return resultsContent.textContent
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-      resultsContent.textContent = "You lose! Scissors beats paper!"
-      return resultsContent.textContent
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-      resultsContent.textContent = "You lose! Rock beats scissors!"
-      return resultsContent.textContent
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-      resultsContent.textContent = "You win! Scissors beats paper!"
-      return resultsContent.textContent
-    }
-  };
-
-
-
-const score = document.querySelector("#score-container")
+  if (playerSelection == computerSelection) {
+    resultsContent.textContent = `It's a tie this round! You both chose ${playerSelection}!`
+    playerSymbol.replaceWith(playerSymbolContent);
+    computerSymbol.replaceWith(computerSymbolContent);
+    return resultsContent.textContent
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    resultsContent.textContent = "You lose this round! Paper beats Rock!"
+    playerSymbol.replaceWith(playerSymbolContent);
+    computerSymbol.replaceWith(computerSymbolContent);
+    return resultsContent.textContent
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    resultsContent.textContent = "You win this round! Rock beats scissors!"
+    playerSymbol.replaceWith(playerSymbolContent);
+    computerSymbol.replaceWith(computerSymbolContent);
+    return resultsContent.textContent
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    resultsContent.textContent = "You win this round! Paper beats rock!"
+    playerSymbol.replaceWith(playerSymbolContent);
+    computerSymbol.replaceWith(computerSymbolContent);
+    return resultsContent.textContent
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    resultsContent.textContent = "You lose this round! Scissors beats paper!"
+    playerSymbol.replaceWith(playerSymbolContent);
+    computerSymbol.replaceWith(computerSymbolContent);
+    return resultsContent.textContent
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    resultsContent.textContent = "You lose this round! Rock beats scissors!"
+    playerSymbol.replaceWith(playerSymbolContent);
+    computerSymbol.replaceWith(computerSymbolContent);
+    return resultsContent.textContent
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    resultsContent.textContent = "You win this round! Scissors beats paper!"
+    playerSymbol.replaceWith(playerSymbolContent);
+    computerSymbol.replaceWith(computerSymbolContent);
+    return resultsContent.textContent
+  }
+};
 
 const scoreContent = document.createElement("div");
 scoreContent.classList.add("score-content");
-  
 score.appendChild(scoreContent);
-
 
 function keepScore() {
   const playerSelection = userChoice;
   const computerSelection = getComputerChoice();
+  computerSymbolContent.src = `${computerSelection}.png`
   let oneRound = playRound(playerSelection, computerSelection)
   if (oneRound.includes("win")) {
     playerScore++;
@@ -93,18 +107,15 @@ function keepScore() {
   }
 };
 
-const finalResult = document.querySelector("#final-result")
-
 const finalResultContent = document.createElement("div");
 finalResultContent.classList.add("final-result-content");
-
 finalResult.appendChild(finalResultContent);
 
 function game() {
   keepScore()
   if (playerScore == 5) {
-    finalResultContent.textContent = `Humans have triumphed! 
-      The final score is Humans: ${playerScore}, Machines: ${computerScore}`
+    finalResultContent.textContent = `You are the Rock Paper Scissors champion! 
+      The final score is You: ${playerScore}, Computer: ${computerScore}`
     resultsContent.textContent = "";
     scoreContent.textContent = "";
     for (let i = 0; i < buttons.length; i++) {
@@ -112,12 +123,12 @@ function game() {
     }
   }
   else if (computerScore == 5) {
-    finalResultContent.textContent = `The machines have won and humanity is doomed...
-      The final score is Humans: ${playerScore}, Machines: ${computerScore}`
+    finalResultContent.textContent = `You lost, but don't worry there's always next year!
+      The final score is You: ${playerScore}, Computer: ${computerScore}`
     resultsContent.textContent = "";
     scoreContent.textContent = "";
     for (let i = 0; i < buttons.length; i++) {
       buttons[i].disabled = true;
     }
   }
-}
+};
